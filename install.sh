cc75c2ff2a74181add71a7f0a3b49ced9ffa2c49
Leandro (Hey Man) <leandro@leandroleite.info>
2019-10-21 10:55:35 -0300

    feat: add zsh + docker

--- install.sh
+++ install.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
-apt install vim git curl terminator tig -y
+apt install vim git curl terminator tig zsh -y
 
+# OH MY ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# NVM
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh)"
+
+# NODE + NPM
+nvm install stable
+nvm use stable
+
+# Z command
 echo "source ~/.dotfiles/z.sh" >> ~/.zshrc
+
+# Alias
 echo "source ~/.dotfiles/alias.sh" >> ~/.zshrc
 
-source config/chrome.sh
-source config/vscode.sh
-source config/docker.sh
-source config/nvm.sh
-source config/npm.sh
-source config/git.sh
-source config/spotify.sh
+# Docker
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
