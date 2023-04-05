656d925b02820ffaa222e75b2c887eba7d549c5f
Leandro (Hey Man) <leandro@leandroleite.info>
2023-04-05 10:20:47 -0300

    add docker dependency

--- install.sh
+++ install.sh
@@ -50,29 +50,30 @@ nvm install stable
 nvm use stable
 
 # Docker
-sudo apt remove docker docker-engine docker.io containerd runc -y
-sudo apt update
-sudo apt install \
+sudo apt-get update
+sudo apt-get install \
     ca-certificates \
     curl \
-    gnupg \
-    lsb-release -y
+    gnupg
 
-sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo usermod -aG docker $USER
+
 sudo service docker start
+
 echo '[boot]
 command="service docker start"' > /etc/wsl.conf
-
 
 mkdir ~/projects
 
