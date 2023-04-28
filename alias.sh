7c17f0726e7f6f5a4048c4f8cef25e2e46b301a1
Leandro (Hey Man) <leandro@leandroleite.info>
2023-04-28 10:44:47 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,9 +1,2 @@
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/heyman/google-cloud-sdk/path.zsh.inc' ]; then . '/home/heyman/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/home/heyman/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/heyman/google-cloud-sdk/completion.zsh.inc'; fi
-alias python=python3
-alias pip=pip3
-alias VENV="source venv/bin/activate"
-alias nr='npm run'
+# fix of : "SignatureDoesNotMatch: Signature expired is now earlier than...."
+alias fix-date="sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\""
